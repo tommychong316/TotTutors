f4bfa2ad6df4d6ca6bf1d7a17f142c43e0b7e533
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-05-16 12:23:47 -0400

    added urls and get endpoints in views/urls files

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import RegisterView, MyTokenObtainPairView
+from authentication import views
+
 
 urlpatterns = [
     path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('tutors/', views.get_all_tutors), 
+    path('tutees/', views.get_all_tutees),
 ]
+
--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth import get_user_model
-from .serializers import MyTokenObtainPairSerializer, RegistrationSerializer
+from .serializers import MyTokenObtainPairSerializer, RegistrationSerializer, OtherUserSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+
+
+
+
 User = get_user_model()
 
 
@@ -14,3 +20,24 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_tutees(request):
+    # use filter to get back only tutors
+    # copy whole method for only tuttees
+    tutees = User.objects.filter(is_tutee=True)
+    # use OtherUserSerializer
+    serializer = OtherUserSerializer(tutees, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_tutors(request):
+    # use filter to get back only tutors
+    # copy whole method for only tuttees
+    tutors = User.objects.filter(is_tutor=True)
+    # use OtherUserSerializer
+    serializer = OtherUserSerializer(tutors, many=True)
+    return Response(serializer.data)
