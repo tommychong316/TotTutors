74b81f14c88a4fd33d09b414fc5c47a8b3224ffc
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-05-16 16:46:57 -0400

    added apps to installed apps in settings

--- backend/comments/urls.py
+++ backend/comments/urls.py
@@ -4,6 +4,5 @@ from comments import views
 urlpatterns = [
     path('', views.user_comments),
     path('all/', views.get_all_comments),
-    path('<int:id>/', views.comment_detail),
-    
+    path('<int:id>/', views.comment_detail),  
 ]
