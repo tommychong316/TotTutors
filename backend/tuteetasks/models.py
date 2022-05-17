a5888bd7531ea25141e495a84267cdf5c8b7ab62
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-05-17 16:09:54 -0400

    created models and serializers for tuteetasks app

--- backend/tuteetasks/models.py
+++ backend/tuteetasks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from authentication.models import User 
 
 # Create your models here.
+class TuteeTask (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.CharField(max_length=255)
+    is_completed = models.BooleanField('completed status', default=False)
---
+++ backend/tuteetasks/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import TuteeTask
+
+class TuteeTaskSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = TuteeTask
+        fields = ["id", "user_id", "task", "is_completed"]
+        depth = 1
+
+user_id = serializers.IntegerField(write_only = True)
