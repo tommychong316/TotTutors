fdf040648452960084245ffe034dce4f59a1b9e8
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-05-24 16:34:55 -0400

    added tuteetasks to Home page

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -5,6 +5,11 @@ import axios from "axios";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import DisplayTutors from "../../components/DisplayTutors/DisplayTutors";
 import DisplayTutees from "../../components/DisplayTutees/DisplayTutees";
+import TuteetaskForm from "../../components/Tasks/TuteetaskForm";
+import DisplayTuteetasks from "../../components/Tasks/DisplayTuteeTasks";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 const HomePage = () => {
   // The "user" value from this Hook contains the decoded logged in user information (username, first name, id)
   // The "token" value is the JWT token that you will send in the header of any request requiring authentication
@@ -14,6 +19,41 @@ const HomePage = () => {
   const [searchTerm, setSearchTerm] = useState([""]);
   const [tutors, setTutors] = useState([]);
   const [tutees, setTutees] = useState([]);
+  const [tuteetasks, setTuteetasks] = useState([]);
+
+  useEffect(() => {
+    getTuteetasks();
+  }, []);
+
+  // do axios call here for tasks
+  const getTuteetasks = async () => {
+    try {
+      let response = await axios.get("http://127.0.0.1:8000/api/tuteetasks/", {
+        headers: {
+          Authorization: "Bearer " + token,
+        }
+      }) 
+      setTuteetasks(response.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const addTuteetask = async (addNew) => {
+    try {
+      await axios.post("http://127.0.0.1:8000/api/tuteetasks/", addNew, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      getTuteetasks();
+    } catch (error) {
+      console.log(addNew);
+      console.log(error.message);
+    }
+  };
+
+
   useEffect(() => {
     getAllTutees();
   }, []);
@@ -61,13 +101,22 @@ const HomePage = () => {
   }, [token]);
   return (
     <div className="container">
-      <h1>Home Page for {user.username}!</h1>
+      <h1>Welcome {user.username}!</h1>
       {cars &&
         cars.map((car) => (
           <p key={car.id}>
             {car.year} {car.make} {car.model}
           </p>
         ))}
+        <div className="col-md-6">
+      <div className="border-box">
+      <TuteetaskForm user={user} addTuteetask={addTuteetask} />  
+      </div></div>
+      <div className="row">
+    <div className="col-md-6">
+      <div className="border-box"></div>
+      <DisplayTuteetasks user={user} parentTuteetasks={tuteetasks} addTuteetask={addTuteetask} getTuteetasks={getTuteetasks}/></div>
+      </div>
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <div className="border-box">
         <DisplayTutors
@@ -89,5 +138,4 @@ const HomePage = () => {
     </div>
   );
 };
-
 export default HomePage;
