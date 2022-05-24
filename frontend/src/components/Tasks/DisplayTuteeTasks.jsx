5a9053b0375193a1e91df9419bf33146970c1e27
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-05-24 16:33:31 -0400

    created a display for Tutee Tasks

---
+++ frontend/src/components/Tasks/DisplayTuteeTasks.jsx
@@ -0,0 +1,34 @@
+import CustomButton from "../CustomButton/CustomButton";
+import axios from "axios";
+import React from "react";
+
+const DisplayTuteetasks = ({ parentTuteetasks }) => {
+  return (
+    <table className="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th>Tasks</th>
+          <th>IsComplete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {parentTuteetasks.map((tuteetask) => {
+          return (
+            <tr key={tuteetask.id}>
+              <tr>
+                <td>{tuteetask.task}</td>
+              </tr>
+              <tr>
+                <td>
+                  <CustomButton message="✔️" />
+                </td>
+              </tr>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default DisplayTuteetasks;
