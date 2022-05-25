1cbb9a05493dbe25437a835e2ac848932b6b9c68
tommychong316 <100431028+tommychong316@users.noreply.github.com>
2022-05-25 10:21:30 -0400

    removed a serializer field.

--- backend/tuteetasks/serializers.py
+++ backend/tuteetasks/serializers.py
@@ -5,7 +5,7 @@ class TuteeTaskSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = TuteeTask
-        fields = ["id", "user_id", "task", "is_completed", "username"]
+        fields = ["id", "user_id", "task", "is_completed",]
         depth = 1
 
 user_id = serializers.IntegerField(write_only = True)
